0b3aa4a22cd986fa1a8f1d302e11e81363800d2b
mec <mec@cl1-2-11.mec.ac.in>
2019-08-20 00:54:44 -0400

    Ordered Repository

---
+++ Cylce1 - Lexical_Analysis/Trials/program.c
@@ -0,0 +1,16 @@
+
+void main()
+{
+	int Integer_Var;
+	char Character_Var;
+	for(int i = 0 ; i < 10 ; )
+	{
+		printf("Hello World\n");
+		i++;
+	}
+	printf("Enter a Number: ");
+	scanf("%d",&Integer_Var);
+
+	Message(Integer_Var)
+	return 0
+}
